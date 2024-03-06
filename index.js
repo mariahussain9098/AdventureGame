48b59a3ddc82f296574e5fa70b27cd783dada53b
wma9969 <49679579+mariahussain9098@users.noreply.github.com>
2024-03-06 15:50:30 +0500

    Add files via upload
    All files are added

---
+++ index.js
@@ -0,0 +1,58 @@
+#!/usr/bin/env node
+// Simple Calculator Using Inquirer Library
+import inquirer from "inquirer";
+async function performOperation() {
+    const ans = await inquirer.prompt([
+        {
+            type: "number",
+            name: "number1",
+            message: "Please enter your first number",
+        },
+        {
+            type: "number",
+            name: "number2",
+            message: "Please enter your second number",
+        },
+        {
+            type: "list",
+            name: "operator",
+            choices: ["Addition", "Subtraction", "Multiplication", "Division"],
+            message: "Please select your operator",
+        },
+    ]);
+    const { number1, number2, operator } = ans;
+    if (number1 && number2 && operator) {
+        let result = 0;
+        if (operator === "Addition") {
+            result = number1 + number2;
+        }
+        else if (operator === "Subtraction") {
+            result = number1 - number2;
+        }
+        else if (operator === "Multiplication") {
+            result = number1 * number2;
+        }
+        else if (operator === "Division") {
+            result = number1 / number2;
+        }
+        console.log(`Your answer is ${result}`);
+        const answer = await inquirer.prompt([
+            {
+                type: "confirm",
+                name: "continue",
+                message: "Do you want to perform another operation?",
+                default: true,
+            },
+        ]);
+        if (answer.continue) {
+            await performOperation();
+        }
+        else {
+            console.log("Exiting the program.");
+        }
+    }
+    else {
+        console.log("Kindly enter valid input");
+    }
+}
+performOperation();
